8347c1a359e503483686f759e1082ddda57fb3ac
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2023-09-14 17:33:59 +0100

    update settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'INTERVENE platform documentation',
+  //tagline: 'Dinosaurs are cool',
+  //favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
   url: 'https://docs.intervenegeneticscores.org',
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ebi-gdp', // Usually your GitHub org/user name.
+  projectName: 'platform-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -59,15 +59,15 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Platform documentation',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'INTERVENE Logo',
           src: 'img/logo.svg',
         },
         items: [
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
+            href: 'https://test.intervenegeneticscores.org',
+            label: 'INTERVENE platform',
             position: 'right',
           },
         ],
